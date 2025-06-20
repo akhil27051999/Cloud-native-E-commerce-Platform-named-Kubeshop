7797283baf365d92a80a791122b6b9353ba9436e
Ubuntu <ubuntu@ip-172-31-41-109.ec2.internal>
2025-06-20 19:01:03 +0000

    updated microservices

--- microservices/product/app.py
+++ microservices/product/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Welcome to the Auth Service"})
+    return jsonify({"message": "Welcome to the Product Service"})
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "auth service is healthy"})
+    return jsonify({"status": "product service is healthy"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
